cafc61f2474a9b2310d129467fadba987fdfebe6
code9102 <code9102@gmail.com>
2026-07-10 12:38:59 +0530

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -3,6 +3,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Learning git now");
 		System.out.println("Learning git now");
+		System.out.println("Trying to pull the code");
 	}
 
 }
